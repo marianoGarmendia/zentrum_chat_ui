1e3dd693ea054aa2363d7a1350a2dbede1183735
Mariano Garmendia <marianogarmendia77@gmail.com>
2025-04-17 11:25:36 -0300

    title layout changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,8 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "Agent Inbox",
-  description: "Agent Inbox UX by LangChain",
+  title: "Agent IA",
+  description: "Agent IA - FaceApp",
 };
 
 export default function RootLayout({
